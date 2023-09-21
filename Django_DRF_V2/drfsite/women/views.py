04adddf67a4fbc962549ee1e12a4c0b329e30ece
Oleksii kozulia <mistosisi@gmail.com>
2023-09-21 15:36:38 +0300

    finish Django DRF + test-drf to fix sum bugs

--- Django_DRF_V2/drfsite/women/views.py
+++ Django_DRF_V2/drfsite/women/views.py
@@ -10,18 +10,25 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
 
  
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer 
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = WomenAPIListPagination
 
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView): 
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-    permission_classes = (IsOwnerOrReadOnly, )
-    # permission_classes = (IsAuthenticated, )
+    # permission_classes = (IsOwnerOrReadOnly, )
+    permission_classes = (IsAuthenticated, )
     # authentication_classes = (TokenAuthentication, )
 
 class WomenAPIDestroy(generics.RetrieveDestroyAPIView): 
