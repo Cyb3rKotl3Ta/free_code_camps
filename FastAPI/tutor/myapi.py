e96ec32b424dd4d907cd2b181e689359b61030ca
Oleksii kozulia <mistosisi@gmail.com>
2024-09-16 17:52:16 +0300

    FastAPI path and query parameters

--- FastAPI/tutor/myapi.py
+++ FastAPI/tutor/myapi.py
@@ -1,4 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 
+students = {
+    1: {
+        'name': 'Jhon',
+        'age': 17,
+        'class': 'year 11',
+    },
+}
+print(len(students))
+
+@app.get('/')
+def index():
+    return {'name': 'First Data'}
+
+@app.get('/get-student/{student_id}')
+def get_student(student_id: int = Path(description='ID of Student', gt=0, le=len(students))):
+    return students[student_id]
+
+@app.get('/get-by-name')
+def get_student(name: Optional[str] = None, test: int):
+    for student_id in students:
+        if students[student_id]['name'] == name:
+            return students[student_id]
+    return {'Data not found'}
