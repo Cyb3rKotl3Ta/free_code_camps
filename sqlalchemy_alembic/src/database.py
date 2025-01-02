78f2f9034608b78afdd0c827ea1dae3b8585f876
Oleksii kozulia <mistosisi@gmail.com>
2025-01-02 15:50:33 +0200

    reverse to default

--- sqlalchemy_alembic/src/database.py
+++ sqlalchemy_alembic/src/database.py
@@ -14,33 +14,29 @@ sync_engine = create_engine(
     # max_overflow=10,
 )
 
-with sync_engine.connect() as conn:
-    res = conn.execute(text("SELECT VERSION()"))
-    print(f'PostgreSQL ver: {res}')
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=True,
+)
 
-# async_engine = create_async_engine(
-#     url=settings.DATABASE_URL_asyncpg,
-#     echo=True,
-# )
+session_factory = sessionmaker(sync_engine)
+async_session_factory = async_sessionmaker(async_engine)
 
-# session_factory = sessionmaker(sync_engine)
-# async_session_factory = async_sessionmaker(async_engine)
+str_256 = Annotated[str, 256]
 
-# str_256 = Annotated[str, 256]
+class Base(DeclarativeBase):
+    type_annotation_map = {
+        str_256: String(256)
+    }
 
-# class Base(DeclarativeBase):
-#     type_annotation_map = {
-#         str_256: String(256)
-#     }
+    repr_cols_num = 3
+    repr_cols = tuple()
 
-#     repr_cols_num = 3
-#     repr_cols = tuple()
+    def __repr__(self):
+        """Relationships не используются в repr(), т.к. могут вести к неожиданным подгрузкам"""
+        cols = []
+        for idx, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or idx < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
 
-#     def __repr__(self):
-#         """Relationships не используются в repr(), т.к. могут вести к неожиданным подгрузкам"""
-#         cols = []
-#         for idx, col in enumerate(self.__table__.columns.keys()):
-#             if col in self.repr_cols or idx < self.repr_cols_num:
-#                 cols.append(f"{col}={getattr(self, col)}")
-
-#         return f"<{self.__class__.__name__} {', '.join(cols)}>"
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
